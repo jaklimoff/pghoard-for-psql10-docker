9c54c150ff5ecaee15e76cc0c9975f8cd18109e7
Jack Klimov <jaklimoff@gmail.com>
2020-01-03 16:26:40 +0800

    update script

--- backup_to_azure.sh
+++ backup_to_azure.sh
@@ -10,7 +10,7 @@ fi
 # shellcheck disable=SC2155
 export DATE=`date +%d-%m-%y_%H:%M`
 
-pg_dump -U commodities > ~/backup.bak
+pg_dump -h $PGHOST -U commodities > ~/backup.bak
 
 az storage blob upload -f backup.bak \
                        --account-name $AZURE_ACCOUNT_NAME \
