a44391618d57f72244846010b44703b6fad81852
Jack Klimov <jaklimoff@gmail.com>
2020-01-03 16:22:02 +0800

    update script

--- backup_to_azure.sh
+++ backup_to_azure.sh
@@ -7,5 +7,13 @@ if [ -n "$TESTING" ]; then
     exit 0
 fi
 
-pg_dump -h $DATABASE_HOST -U commodities > ~/mainhub.bak
+# shellcheck disable=SC2155
+export DATE=`date +%d-%m-%y_%H:%M`
 
+pg_dump -U commodities > ~/backup.bak
+
+az storage blob upload -f backup.bak \
+                       --account-name $AZURE_ACCOUNT_NAME \
+                       --account-key $AZURE_ACCOUNT_KEY \
+                       -c $AZURE_CONTAINER_NAME \
+                       -n db-${DATE}.bak
